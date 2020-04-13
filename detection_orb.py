ab20d92c20ca6f897421bf28c6cb61e0d85f101f
Ivancrto <ivancrtoc@gmail.com>
2020-04-13 12:55:03 +0200

    Flann indes lsh

--- detection_orb.py
+++ detection_orb.py
@@ -9,9 +9,11 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-
+#AQUI CREAMOS TODAS LAS ESTRUCTURAS QUE VAYAMOS A USAR
 imagenes_train = []
 informacion_train = []
+
+
 
 #1. En primer lugar hará falta programar un bucle que cargue las imágenes de training. 
 def cargarImagen(dateImage):
@@ -24,6 +26,16 @@ def cargarImagen(dateImage):
 #2. Utilizar la clase cv2.ORB_create para obtener los keypoints y los descriptores de cada imagen. 
 detector = cv2.ORB_create(100, 1.1, 1) #keypoints	scaleFactor    nlevels(pirámide) 
    
+#CREAMOS un FlannBasedMatcher utilizando la distancia de Hamming
+FLANN_INDEX_LSH = 6
+#En la especificacion de OPENCV, pone los siguientes valores, a lo mejor los tenemos que modificar
+index_params= dict(algorithm = FLANN_INDEX_LSH,
+                   table_number = 6, # 12
+                   key_size = 12,     # 20
+                   multi_probe_level = 1) #2
+search_params = dict(checks=-1) # Maximum leafs to visit when searching for neighbours.
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+
          
 def main():
     print("Clase principal")
